5d97900cca5c716398207c67d5aaae2072cd6ca8
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2022-07-03 21:26:03 +0100

    feat: implement integration to get data from endpoint

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,50 @@
 <template>
   <TheHeader />
-  <TheTable :data="tableData" />
+  <BaseLoader v-if="isLoading" />
+  <TheTable v-else :data="tableData" />
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
 import TheTable from "./components/TheTable.vue";
+import BaseLoader from "./components/BaseLoader.vue";
 
 export default {
   components: {
     TheHeader,
     TheTable,
+    BaseLoader,
   },
   data() {
     return {
+      isLoading: false,
       tableData: {
         headers: ["name", "email", "phone-number"],
-        rows: [
-          {
-            name: "Peter Lawanson",
-            email: "oriade1@gmail.com",
-            "phone-number": "+234 000 0000",
-          },
-          {
-            name: "Peter Lawanson",
-            email: "oriade1@gmail.com",
-            "phone-number": "+234 000 0000",
-          },
-          {
-            name: "Peter Lawanson",
-            email: "oriade1@gmail.com",
-            "phone-number": "+234 000 0000",
-          },
-          {
-            name: "Peter Lawanson",
-            email: "oriade1@gmail.com",
-            "phone-number": "+234 000 0000",
-          },
-          {
-            name: "Peter Lawanson",
-            email: "oriade1@gmail.com",
-            "phone-number": "+234 000 0000",
-          },
-          {
-            name: "Peter Lawanson",
-            email: "oriade1@gmail.com",
-            "phone-number": "+234 000 0000",
-          },
-          {
-            name: "Peter Lawanson",
-            email: "oriade1@gmail.com",
-            "phone-number": "+234 000 0000",
-          },
-          {
-            name: "Peter Lawanson",
-            email: "oriade1@gmail.com",
-            "phone-number": "+234 000 0000",
-          },
-          {
-            name: "Peter Lawanson",
-            email: "oriade1@gmail.com",
-            "phone-number": "+234 000 0000",
-          },
-          {
-            name: "Peter Lawanson",
-            email: "oriade1@gmail.com",
-            "phone-number": "+234 000 0000",
-          },
-        ],
+        rows: [],
         actionsExist: true,
       },
     };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      this.isLoading = true;
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      await response.json().then((data) => {
+        data.forEach((row) => {
+          this.tableData.rows.push({
+            name: row.name,
+            email: row.email,
+            "phone-number": row.phone,
+          });
+        });
+        this.isLoading = false;
+      });
+    },
   },
 };
 </script>
---
+++ src/components/BaseLoader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="w-12 h-12 mx-auto my-12 border border-solid border-t-transparent border-x-button-background border-b-button-background rounded-full"
+  />
+</template>
+
+<style scoped>
+div {
+  animation: spin 1.5s infinite linear;
+}
+@keyframes spin {
+  100% {
+    transform: rotate(1turn);
+  }
+}
+</style>
